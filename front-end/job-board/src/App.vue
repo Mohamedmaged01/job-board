7fec814fdc507f40be6decfddf9a5b17f67ed7c8
ahmed <amhmdslah104@gmail.com>
2025-05-05 22:22:33 +0300

    candidate configration with sidear

--- front-end/job-board/src/App.vue
+++ front-end/job-board/src/App.vue
@@ -1,30 +1,35 @@
+<!-- customize routes -->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <nav>
+      <!-- <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink> -->
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+nav {
+  padding: 1rem;
+  background: #f0f0f0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+nav a {
+  margin-right: 1rem;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+nav a.router-link-active {
+  font-weight: bold;
 }
 </style>
+>
---
+++ front-end/job-board/src/views/candidate/CandidateOverview.vue
@@ -0,0 +1,16 @@
+<script scoped>
+</script>
+<template>
+  <div class="candidate-overview">
+    <div class="content">
+      
+      <h1>Candidate Overview</h1>
+      <!-- Add your content here -->
+    </div>
+  </div>
+</template>
+<style scoped>
+.candidate-overview {
+  display: flex;
+}
+</style>
